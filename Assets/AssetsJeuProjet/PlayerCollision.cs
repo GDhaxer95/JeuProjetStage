10bc3683ab369c7a8fe83a93f3d903a2e349238f
GDhaxer95 <60401785+GDhaxer95@users.noreply.github.com>
2021-06-23 17:49:53 +0200

    ajout de la collision ( a voir)
    j'ai ajouter la collision entre platform et entre un obstacle hostile

---
+++ Assets/AssetsJeuProjet/PlayerCollision.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollision : MonoBehaviour {
+
+	void OnCollisionEnter2D (Collision2D col){
+		Debug.Log ("We hit something");
+		if (col.gameObject.name == "Obstacle"){
+			Destroy (this.gameObject);
+			Debug.Log ("You lost");
+			
+		}
+	}
+
+
+}
