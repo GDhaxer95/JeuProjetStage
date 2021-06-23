8df77f78c8d779be093a9933efbc3ae3c8d9a9f6
GDhaxer95 <60401785+GDhaxer95@users.noreply.github.com>
2021-06-23 17:59:08 +0200

    update des fichiers scene et du script

--- Assets/AssetsJeuProjet/PlayerCollision.cs
+++ Assets/AssetsJeuProjet/PlayerCollision.cs
@@ -5,7 +5,7 @@ public class PlayerCollision : MonoBehaviour {
 
 	void OnCollisionEnter2D (Collision2D col){
 		Debug.Log ("We hit something");
-		if (col.gameObject.name == "Obstacle"){
+		if (col.gameObject.tag == "Obstacle"){
 			Destroy (this.gameObject);
 			Debug.Log ("You lost");
 			
