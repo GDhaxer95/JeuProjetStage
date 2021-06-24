d2768c35bb97b8de7398366625ff45b45f01c633
GDhaxer95 <60401785+GDhaxer95@users.noreply.github.com>
2021-06-24 17:57:42 +0200

    update des differents fichiers, et ajout de scripts.

---
+++ Assets/AssetsJeuProjet/CameraFollow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+	public Vector3 offset;
+	public float smoothFactor = 10f;
+	Camera cam;
+
+	public void Start(){
+		
+		cam = Camera.main;
+	}
+	public void Update(){
+
+		if (Input.GetKey (KeyCode.KeypadPlus))
+			cam.orthographicSize -= .1f;
+		if (Input.GetKey (KeyCode.KeypadMinus))
+			cam.orthographicSize += .1f;
+	}
+
+	private void FixedUpdate()
+	{
+		Follow ();
+	}
+
+	void Follow(){
+		
+		Vector3 targetPosition = target.position + offset;
+		Vector3 smoothedPosition = Vector3.Lerp (transform.position, targetPosition, smoothFactor*Time.fixedDeltaTime);
+		transform.position = targetPosition;
+	}
+
+	
+}
--- Assets/AssetsJeuProjet/PlayerCollision.cs
+++ Assets/AssetsJeuProjet/PlayerCollision.cs
@@ -3,11 +3,12 @@ using System.Collections;
 
 public class PlayerCollision : MonoBehaviour {
 
+
 	void OnCollisionEnter2D (Collision2D col){
 		Debug.Log ("We hit something");
 		if (col.gameObject.tag == "Obstacle"){
-			Destroy (this.gameObject);
-			Debug.Log ("You lost");
+			FindObjectOfType<GameManager>().EndGame();
+
 			
 		}
 	}
